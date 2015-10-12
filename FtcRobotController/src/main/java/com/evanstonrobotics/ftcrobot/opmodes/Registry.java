d7664a823794e14fcdb24ec038d42ae445366e26
Alex Brooke <akb.sbc@gmail.com>
2015-10-12 15:23:26 -0500

    initial commit

---
+++ FtcRobotController/src/main/java/com/evanstonrobotics/ftcrobot/opmodes/Registry.java
@@ -0,0 +1,22 @@
+package com.evanstonrobotics.ftcrobot.opmodes;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Alex
+ * A registry containing a map of our custom opmodes ("Name"=>Name.class)
+ * {@see com.qualcomm.ftcrobotcontroller.opmodes.FtcOpModeRegister#register(OpModeManager)}
+ */
+public class Registry {
+    /**
+     * Whenever a new opmode is created, add it to this list so it can be registered by the
+     * controller activity
+     */
+    public static final Map<String, Class> REGISTRY;
+    static {
+        REGISTRY = new HashMap<>();
+        REGISTRY.put("TestOpMode", TestOpMode.class);
+
+    }
+}
---
+++ FtcRobotController/src/main/java/com/evanstonrobotics/ftcrobot/opmodes/TestOpMode.java
@@ -0,0 +1,19 @@
+package com.evanstonrobotics.ftcrobot.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * @author Alex
+ */
+public class TestOpMode extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
