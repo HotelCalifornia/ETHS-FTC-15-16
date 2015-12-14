6ba65ef85b14360b12c91e4a20ef5538bfad6360
Alex Brooke <akb.sbc@gmail.com>
2015-12-14 12:41:09 -0600

    Initial layout for mountain climbing code

--- FtcRobotController/src/main/java/com/evanstonrobotics/ftcrobot/opmodes/Test0OpMode.kt
+++ FtcRobotController/src/main/java/com/evanstonrobotics/ftcrobot/opmodes/Test0OpMode.kt
@@ -14,13 +14,27 @@ public class Test0OpMode : OpMode() {
     private val motorLeft : DcMotor = hardwareMap.dcMotor.get("motorLeft")
     private val motorRight : DcMotor = hardwareMap.dcMotor.get("motorRight")
 
+    private val X = 0
+    private val Y = 1
+    private val Z = 2
+
     override fun init() {
         motorLeft.direction = DcMotor.Direction.REVERSE
     }
 
     override fun loop() {
         var accel = SensorService.accelerometer
-        telemetry.addData("accelerometer", "(${accel[0]}, ${accel[1]}, ${accel[2]}")
+        telemetry.addData("accelerometer", "(${accel[X]}, ${accel[Y]}, ${accel[Z]}")
+
+        motorLeft.power = 0.5
+        motorRight.power = 0.5
+
+        //Do testing to confirm values and axes
+        if(accel[X] > 1) {
+            //do testing to confirm power
+            motorLeft.power = 0.75
+            motorRight.power = 0.75
+        }
     }
 
 }
